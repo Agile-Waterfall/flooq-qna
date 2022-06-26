77227bfd9bd49cee18f6b3d401206832663f9f15
Felix Saaro <felix@saaro.ch>
2022-06-26 15:03:30 +0200

    Cleanup api function

--- pages/api/submit-question.js
+++ pages/api/submit-question.js
@@ -1,21 +1,15 @@
 export default async function handler(req, res) {
-  try {
+  const response = await fetch(process.env.FLOOQ_DATAFLOW, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(req.body),
+  })
 
-    const response = await fetch(process.env.FLOOQ_DATAFLOW, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(req.body),
-    })
-
-    if (!response.ok) {
-      res.status(500).json('Failed to send question')
-    } else {
-      res.status(200).json('Success')
-    }
-
-  } catch (e) {
-    res.status(500).json(e)
+  if (!response.ok) {
+    res.status(500).json('Failed to send question')
+  } else {
+    res.status(200).json('Success')
   }
 }
